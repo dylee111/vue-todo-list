e8b174941c5fc89099a9636c88b7ce098805d4d0
dddzzz <dylee0568@gmail.cim>
2023-09-10 23:26:37 +0900

    JSON parsing해서 넘겨야 하나?

--- src/components/AppBody.vue
+++ src/components/AppBody.vue
@@ -1,31 +1,40 @@
 <template>
- <div>
- <div>body</div>
-   <input type="text" v-model="memo">
-   <input type="datetime-local" v-model="date"> <br>
-   <button v-on:click="inputValue">추가</button>
-   <hr>
-   <table id="vue-table">
-     <thead>
-     <tr>
-       <th>MEMO</th>
-       <th>DATE</th>
-     </tr>
-     </thead>
-     <tbody>
+  <div>
+    <div>body</div>
+    <input type="text" v-model="memo">
+    <input type="datetime-local" v-model="date"> <br>
+    <button v-on:click="inputValue">추가</button>
+    <hr>
+    <table id="vue-table">
+      <thead>
       <tr>
-<!--        <td v-for="rows in rows.target"></td>-->
-<!--        <td></td>-->
+        <th>MEMO</th>
+        <th>DATE</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr >
+
 
       </tr>
-     </tbody>
-   </table>
- </div>
+      </tbody>
+    </table>
+    <div >
+      {{ rows }}
+      <hr>
+      {{ info }}
+      <br>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "AppBody",
+  setup() {
+    // const rows = reactive([]);
+
+  },
   data: function () {
     return {
       memo: '',
@@ -43,33 +52,36 @@ export default {
       this.info.memo = this.memo;
       this.info.date = this.date;
 
-      this.rows.push(this.info);
+      let tmpJson = JSON.parse(JSON.stringify(this.info));
+      this.$data.rows.push(tmpJson);
+      let testJson = JSON.parse(JSON.stringify(this.$data.rows));
+
+      testJson.forEach(function (row) {
+        console.log("log");
+        console.log(row.memo);
+      })
+      console.log(testJson);
       this.info = {};
       this.memo = '';
       this.date = '';
 
-
-      const handler = {};
-
-      console.log(this.rows[0]);
-      const proxy = new Proxy(this.rows, handler);
-      console.log(proxy);
-
-      this.addRow();
+      console.log("----1111");
+      // console.log(handler.get(this.$data.rows, this.$data.rows[0]));
 
     },
-    addRow: function () {
-
+    jsonConvert: function (obj) {
+      obj = JSON.parse(JSON.stringify(obj))
+      return obj;
     }
   }
 }
 </script>
 
 <style scoped>
-  table {
-    border: 1px solid black;
-  }
-  th, td {
-    border: 1px solid black;
-  }
+table {
+  border: 1px solid black;
+}
+th, td {
+  border: 1px solid black;
+}
 </style>
